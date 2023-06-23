9cc7899ef00222bfa393767f0415e251f7098a5d
gregolly <gregollyff@hotmail.com>
2023-06-23 11:49:29 -0400

    refactor: updated formattedDate function

--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -1,8 +1,10 @@
 import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 export function formattedDate(value: string) {
   const distance = formatDistanceToNow(new Date(value), {
     addSuffix: true,
+    locale: ptBR,
   })
 
   return distance
